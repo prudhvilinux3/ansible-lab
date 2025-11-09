8bf8bb80cdb5a629f3fedfc032bffc64fcbf69a9
Prudhvi B <prudhvi.linux3@gmail.com>
2025-11-09 06:37:28 +0000

    Modified the get ansible instances to report warning

--- get-ansible-aws.sh
+++ get-ansible-aws.sh
@@ -9,9 +9,15 @@
 
 
 
-
-
+cd inventory/
 echo "[aws]" > inventory
-aws ec2 describe-instances   --filters "Name=tag:Manager,Values=Ansible" "Name=instance-state-name,Values=running"   --query "Reservations[].Instances[].{ID:InstanceId,IP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value}"   --output json   | jq -r '.[] | select(.IP != null) | "\(.Name //  "ec2")_\(.ID) ansible_host=\(.IP)"'   >> inventory
+aws ec2 describe-instances   --filters "Name=tag:Manager,Values=Ansible" "Name=instance-state-name,Values=running"   --query "Reservations[].Instances[].{ID:InstanceId,IP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value}"   --output json   | jq -r '.[] | select(.IP != null) | "\(.Name //  "ec2")_\(.ID) ansible_host=\(.IP)"'   >> /tmp/inven 
+running_instances=`wc -l </tmp/inven`
+
+if [ "$running_instances" -lt 1 ] ; then
+	echo -e "No Instances found running that has tags as Manager = Ansible \n Please check your aws console and scale up the instances as required" 
+else 
+	cat /tmp/inven >> inventory
+fi
 
 
